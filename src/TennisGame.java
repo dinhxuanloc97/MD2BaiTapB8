82bc298a25b242e52d7145c85fa2a626cb8cc906
dinhxuanloc97 <dinhxuanloc741997@gmail.com>
2022-10-06 10:22:06 +0700

    len

---
+++ src/TennisGame.java
@@ -0,0 +1,60 @@
+public class TennisGame {
+    public static String getScore(String player1Name, String player2Name, int fist_score, int second_score) {
+        String score = "";
+        int tempScore=0;
+        if (fist_score==second_score)
+        {
+            switch (fist_score)
+            {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                case 3:
+                    score = "Forty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+            }
+        }
+        else if (fist_score>=4 || second_score>=4)
+        {
+            int minusResult = fist_score-second_score;
+            if (minusResult==1) score ="Advantage player1";
+            else if (minusResult ==-1) score ="Advantage player2";
+            else if (minusResult>=2) score = "Win for player1";
+            else score ="Win for player2";
+        }
+        else
+        {
+            for (int i=1; i<3; i++)
+            {
+                if (i==1) tempScore = fist_score;
+                else { score+="-"; tempScore = second_score;}
+                switch(tempScore)
+                {
+                    case 0:
+                        score+="Love";
+                        break;
+                    case 1:
+                        score+="Fifteen";
+                        break;
+                    case 2:
+                        score+="Thirty";
+                        break;
+                    case 3:
+                        score+="Forty";
+                        break;
+                }
+            }
+        }
+        return score;
+    }
+
+}
